366a6531904e1f9b214b28f36cbda66d4b956b29
Daniel Alarcon <hdalarcon@gmail.com>
2023-09-18 21:13:53 -0300

    Se sube authorization, se quita console.log

--- src/presentation/middlewares/authorization.js
+++ src/presentation/middlewares/authorization.js
@@ -1,10 +1,9 @@
-import { use } from "chai";
-
 const authorization = (permission) =>
 {
     return async(req, res, next) =>
     {
-        console.log('user ',user);
+        const user = req.user;
+
         if(!user.isAdmin && !user.role?.permissions.includes(permission))
         {
             return res.status(401).send({ message: 'Not authorization!'});
