02c936f468a6b27352896e90f0425998954815d8
Daniel Alarcon <hdalarcon@gmail.com>
2023-09-21 20:53:02 -0300

    Se modifica config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 
 const config = {
-    env: process.env.NODE_ENV,
     port: process.env.NODE_PORT,
-    dbUri: process.env.MONGO_DB_URI,
+    dbUri: process.env.DB_URI,
     privateKey: process.env.PRIVATE_KEY,
 }
 
